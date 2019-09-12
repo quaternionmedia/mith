653f4670846a142cbf50648e466ed21c130aa3d2
Harpo <harpo@harpo.me>
2019-09-11 22:52:21 -0700

    fixed multiple tag passing on contact form

--- src/Contact.js
+++ src/Contact.js
@@ -2,18 +2,20 @@ import m from 'mithril';
 
 module.exports = {
   view: function(vnode) {
-    return
-    m('section#contact', [
+    return m('section#contact[class=container]', [
       m('h2', 'Contact us!'),
       m('p', 'Seriously! Hire us!'),
       m('form#form', [
         m('label[for=name]', 'Name'),
-        m('input#name[type=text,name=name]', 'Your name'),
+        m('input#name', {type:'text', name:'name', placeholder: 'your name'}),
+        m('br'),
         m('label[for=email]', 'Email'),
-        m('input#email[for=email]', 'Your email'),
+        m('input#email', {
+          type:'text', name:'email', placeholder: 'your email'}, 'Your email'),
+        m('br'),
         m('label[for=message]', 'Message'),
-        m('input[type=text,name=message]', 'Your message here'),
-        m('input[type=submit,value=Submit]')
+        m('input#message', {type:'text', name:'message', placeholder: 'your message'}),
+        m('input', {type:'submit',value:'Submit'})
       ])
     ])
   }
